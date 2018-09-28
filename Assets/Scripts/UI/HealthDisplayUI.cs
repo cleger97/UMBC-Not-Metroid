2128768e4cd0d58647203e447e485781b58fce6a
Micah DeBruno <32474022+micahdebruno@users.noreply.github.com>
2018-09-28 09:31:31 -0400

    Health Bar
    Added a simple health bar. Press 'p' to simulate taking damage.

---
+++ Assets/Scripts/UI/HealthDisplayUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthDisplayUI : MonoBehaviour {
+
+    public Slider healthBar;
+    [SerializeField]
+    private int health = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthBar.value = health;
+        //for testing
+        if (Input.GetKeyDown(KeyCode.P))
+            health--;
+	}
+}
