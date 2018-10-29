1c969de7b4c634343d7a70409ff1493f63520221
Micah DeBruno <32474022+micahdebruno@users.noreply.github.com>
2018-10-29 19:56:54 -0400

    changes

---
+++ Assets/Scripts/Terrain/BoxButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxButton : MonoBehaviour {
+    [SerializeField]
+    private GameObject block;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Weapon")
+        {
+            Instantiate(block, new Vector3(transform.position.x + 1.5f, transform.position.y + 8, transform.position.z), Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
