832d0a5d4f7432ef9a0a3c5ad8a77e65e7405240
Micah DeBruno <32474022+micahdebruno@users.noreply.github.com>
2019-03-01 15:47:15 -0500

    Prototype of energy regen boost

--- Assets/Scripts/Enemy/Boss/SpiderBossDamage.cs
+++ Assets/Scripts/Enemy/Boss/SpiderBossDamage.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class SpiderBossDamage : MonoBehaviour {
     public int health;
     public ParticleSystem ps1, ps2;
+    private float damageTime;
     [SerializeField]
     private GameObject boss;
+
+    [SerializeField] private SpriteRenderer sr;
     // Use this for initialization
     void Start () {
         health = GameObject.FindObjectOfType<SpiderBoss>().health;
@@ -14,17 +17,21 @@ public class SpiderBossDamage : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        damageTime -= Time.deltaTime;
+        if (damageTime < 0)
+        {
+            this.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D col)
     {
         Vector2 pos = new Vector2(col.transform.position.x, col.transform.position.y);
         if (col.tag == "Weapon" && col.GetComponent<BoxCollider2D>().enabled)
         {
-            
+            damageTime = .1f;
             health--;
             Instantiate(ps1, pos, Quaternion.identity);
-
+            sr.color = Color.black;
         }
         
         if (health <= 0)
---
+++ Assets/Scripts/Pickups/EnergyRegenPickup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyRegenPickup : MonoBehaviour
+{
+    private float regenTime;
+    [SerializeField] private PlayerWeapon regen;
+
+    private SpriteRenderer sr;
+    private bool pickedUp = false;
+    public float regenBoostTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        regenTime -= Time.deltaTime;
+        if (regenTime < 0 && pickedUp)
+        {
+            regen.energyRegen = 1;
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            pickedUp = true;
+            sr.enabled = false;
+            regenTime = regenBoostTime;
+            regen.energyRegen *= 5f;
+        }
+    }
+}
