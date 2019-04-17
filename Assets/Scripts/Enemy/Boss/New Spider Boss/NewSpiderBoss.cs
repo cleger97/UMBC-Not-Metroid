c7c73cd9b37a204729148c4f33bc170c50453dcd
cleger97 <chris.legerjr@gmail.com>
2019-04-17 18:43:15 -0400

    Enemy moving/threaten state/attacking state added
    Working on wiring them all to be functional independently but working nonetheless

--- Assets/Scripts/Enemy/Boss/New Spider Boss/NewSpiderBoss.cs
+++ Assets/Scripts/Enemy/Boss/New Spider Boss/NewSpiderBoss.cs
@@ -8,13 +8,19 @@ public class NewSpiderBoss : MonoBehaviour
     // "Hardened Shield" effect: Should take 10 hits, regardless of strength.
     public int health = 10;
     public int maxHealth = 10;
-    public float speed;
+    public float speed = 5;
     // damage is gonna ramp up the lower the boss is
     public float damage;
     public float atkSpd;
 
+    public bool isThreatened = false;
+
+    public bool isAttacking = false;
+
     private bool flip = false;
     private float flipTime = 0;
+
+    private bool isFacingLeft = true;
 
     private int state = 0;
 
@@ -34,30 +40,38 @@ public class NewSpiderBoss : MonoBehaviour
 
         health = maxHealth;
 
-        healthBar.maxValue = health;
-        healthBar.value = health;
+        if (healthBar != null) {
+            healthBar.maxValue = health;
+            healthBar.value = health;
+        }
+        
 
-        anim.SetTrigger("Idle");
+        ResetAnimator();
+
+        isThreatened = false;
+
+        isFacingLeft = (transform.rotation.y == 180) ? false : true;
 
     }
 
     private void ResetAnimator() {
-        anim.ResetTrigger("Idle");
+        anim.SetBool("Idle", false);
         anim.ResetTrigger("Attack");
-        anim.ResetTrigger("Walk");
+        anim.SetBool("Walk", false);
 
-        anim.SetTrigger("Idle");
-        state = 0;
-        animState = 0;
+        //anim.SetBool("Idle", true);
+        //state = 0;
+        //animState = 0;
     }
 
     private void UpdateAnimator(int state) {
         if (animState == state) {
             return;
         }
+        ResetAnimator();
         switch (state) {
-            case 0: anim.SetTrigger("Idle"); break;
-            case 1: anim.SetTrigger("Walk"); break;
+            case 0: anim.SetBool("Idle", true); break;
+            case 1: anim.SetBool("Walk", true); break;
             case 2: anim.SetTrigger("Attack"); break;
             default: break;
         }
@@ -82,16 +96,50 @@ public class NewSpiderBoss : MonoBehaviour
         switch (state) {
             // idle state
             case 0: {
-                
+                // check if threatened
+                // if so, move towards threat
+                if (isThreatened) {
+                    state = 1;
+                }
                 break;
             }
             // walk at player
             case 1: {
+                // walk at player
+                Vector3 direction = - this.transform.position + player.transform.position;
+                float xDirection = Mathf.Sign(direction.x);
+
+                if (isFacingLeft && (xDirection == 1)) {
+                    // Flip
+                    Quaternion rot = new Quaternion(0, 180, 0, 0);
+                    transform.rotation = rot;
+                    isFacingLeft = false;
+                }                
+                else if (!isFacingLeft && (xDirection == -1)) {
+                    // Flip
+                    Quaternion rot = new Quaternion(0, 0, 0, 0);
+                    transform.rotation = rot;
+                    isFacingLeft = true;
+                }
+
+                Vector3 newPosition = new Vector3(xDirection * speed * Time.deltaTime + transform.position.x, transform.position.y, transform.position.z);
+                transform.position = newPosition;
+                
+                if (!isThreatened) {
+                    state = 0;
+                }
+
+                if (isAttacking) {
+                    state = 2;
+                }
 
                 break;
             }
             // slam attack
             case 2: {
+                // Start attack
+                // When finished will handle itself
+
                 break;
             }
 
@@ -101,4 +149,18 @@ public class NewSpiderBoss : MonoBehaviour
             }
         }
     }
+
+    public void StartAttack() {
+
+    }
+
+    public void ResolveAttack() {
+        if (isThreatened) {
+            state = 1;
+        } else {
+            state = 0;
+        }
+        isAttacking = false;
+    }
+
 }
